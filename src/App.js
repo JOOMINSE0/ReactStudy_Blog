f8775e287fb95c6a9454f1871ee10e78dc170a6e
joominse0 <sally51501512@gmail.com>
2024-04-02 23:39:09 +0900

    Component문법

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {useState} from 'react';
 function App() {
 
   let post = '강남 우동 맛집'
-  let [글제목, 글제목변경] = useState(['여자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
   let [따봉, 따봉변경] = useState(0);
 
   return (
@@ -14,11 +14,14 @@ function App() {
       <h4>blog</h4>
       </div>
 
-      <button>가나다순 정렬</button>
+      <button onClick={() => {
+        let copy = [...글제목];
+        copy.sort();
+        글제목변경(copy) }}>가나다순 정렬</button>
 
       <button onClick={ () => { 
         let copy = [...글제목];
-        copy[0]='남자 코트 추천';
+        copy[0]='여자 코트 추천';
         글제목변경(copy);
      }}>글 수정</button>
 
@@ -34,8 +37,23 @@ function App() {
         <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
+
+      <Modal></Modal>
+      <Modal/>
+
     </div>
   );
+}
+
+function Modal(){
+  return(
+    <div className="modal">
+    <h4>제목</h4>
+    <p>날짜</p>
+    <p>상세내용</p>
+  </div>
+  )
+
 }
 
 export default App;
